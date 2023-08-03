b76056043a659d8b0b80076479fdf02430516279
codepimp15 <31225699+codepimp15@users.noreply.github.com>
2023-08-03 19:23:05 +0200

    Removed tests tags, added another unit test example

---
+++ app/src/test/java/com/example/faunaonboarding/ExampleUnitTest2.kt
@@ -0,0 +1,11 @@
+package com.example.faunaonboarding
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest2 {
+    @Test
+    fun multiplication_isCorrect() {
+        assertEquals(4, 2 * 2)
+    }
+}
