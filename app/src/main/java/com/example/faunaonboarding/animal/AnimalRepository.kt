2f3667c168316a002b340a93eb68658aeac4f270
matic <matic011.jovan@gmail.com>
2023-10-08 19:37:14 +0200

    Modified repositories for testing

--- app/src/main/java/com/example/faunaonboarding/animal/AnimalRepository.kt
+++ app/src/main/java/com/example/faunaonboarding/animal/AnimalRepository.kt
@@ -3,22 +3,18 @@ package com.example.faunaonboarding.animal
 import com.example.faunaonboarding.animalCreate.AnimalCreate
 import javax.inject.Inject
 import javax.inject.Singleton
+import com.example.faunaonboarding.util.Result
 
 @Singleton
 class AnimalRepository @Inject constructor(
     private val animalDataSource : AnimalDataSource
 ) {
-//    suspend fun animalCreate(animalCreate: AnimalCreate): Flow<Result<CreateAnimalMutation.CreateAnimal>> {
-//        return animalDataSource.createAnimal(animalCreate)
-//        requestCodeFlow.emit(Result.success(true))
-//
-//    }
 
-    suspend fun animalCreate(animalCreate: AnimalCreate): com.example.faunaonboarding.util.Result<Boolean> {
+    suspend fun animalCreate(animalCreate: AnimalCreate): Result<Boolean> {
         return animalDataSource.createAnimal(animalCreate)
     }
 
-    suspend fun animalUpdate(animalUpdate: AnimalUpdate): com.example.faunaonboarding.util.Result<Boolean> {
+    suspend fun animalUpdate(animalUpdate: AnimalUpdate): Result<Boolean> {
         return animalDataSource.updateAnimal(animalUpdate)
     }
 }
--- app/src/main/java/com/example/faunaonboarding/login/LoginRepository.kt
+++ app/src/main/java/com/example/faunaonboarding/login/LoginRepository.kt
@@ -1,18 +1,22 @@
 package com.example.faunaonboarding.login
 
 import com.example.faunaonboarding.createAccount.AccountDataSource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
 
 @Singleton
-class LoginRepository @Inject constructor(
+open class LoginRepository @Inject constructor(
     private val accountDataSource: AccountDataSource,
-//    override val coroutineContext: CoroutineContext,
-) {
-//    private val scope = CoroutineScope(coroutineContext)
+    override val coroutineContext: CoroutineContext = Dispatchers.IO,
+) : CoroutineScope {
+    private val scope = CoroutineScope(coroutineContext)
 
     private val requestCodeFlow = MutableSharedFlow<Result<Boolean>>(
         replay = 1,
@@ -21,12 +25,10 @@ class LoginRepository @Inject constructor(
 
     val getRequestCodeFlow: Flow<Result<Boolean>> = requestCodeFlow
 
-    fun requestCode(phone: String): Result<Boolean> {
-//        accountDataSource.requestLoginCode(phone).collect {
-//            requestCodeFlow.emit(it)
-//        }
-        return (Result.success(true))
-
+    open suspend fun requestCode(phone: String) = scope.async {
+        accountDataSource.requestLoginCode(phone).collect {
+            requestCodeFlow.emit(it)
+        }
     }
 
     private val accessCodeFlow = MutableSharedFlow<Result<Boolean>>(
@@ -36,10 +38,9 @@ class LoginRepository @Inject constructor(
 
     val getLoginFlow: Flow<Result<Boolean>> = accessCodeFlow
 
-    suspend fun login(phone: String, accessToken: String): Result<Boolean> {
-//        accountDataSource.login(phone, accessToken).collect {
-//            accessCodeFlow.emit(it)
-//        }
-        return (Result.success(true))
+    open suspend fun login(phone: String, accessToken: String) = scope.async {
+        accountDataSource.login(phone, accessToken).collect {
+            accessCodeFlow.emit(it)
+        }
     }
 }
